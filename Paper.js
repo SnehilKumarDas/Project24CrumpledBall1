c75912ac3523c84eea5bd98acf2ab59659aa7fdc
SnehilKumarDas <72400946+SnehilKumarDas@users.noreply.github.com>
2020-11-01 18:40:04 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,39 @@
+class Paper{
+
+    constructor(x,y,r){
+
+        var options = {
+
+            isStatic : false,
+            restitution : 0.3,
+            friction : 0.5,
+            density : 1.2
+
+        }
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.paper = Bodies.circle(this.x,this.y,this.r/2,options);
+       
+        World.add(world,this.paper);
+        
+
+
+    }
+
+    display(){
+
+        push();
+        translate(100,600)
+        ellipseMode(CENTER);
+        ellipse(0,0,this.r,this.r);
+        pop();
+
+
+
+    }
+
+
+
+
+}
